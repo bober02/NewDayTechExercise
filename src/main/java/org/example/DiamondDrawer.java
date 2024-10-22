cc8852a1dae63683883166193cfbfe812b7b441b
Maciej Bobrowski <msbobrowski@gmail.com>
2024-10-22 15:21:34 +0100

    Initial commit

---
+++ src/main/java/org/example/DiamondDrawer.java
@@ -0,0 +1,62 @@
+package org.example;
+
+// we coudl hide this behind interface but it seems too much of an overkill for a simple task like this
+public class DiamondDrawer {
+
+    public DiamondDrawer() {
+
+    }
+
+    public String drawDiamond(char letter) {
+        if (!Character.isLetter(letter)) {
+            throw new IllegalArgumentException("Character specified is not a letter: " + letter);
+        }
+        // include lowercase too
+        letter = Character.toUpperCase(letter);
+        if (letter > 'Z' || letter < 'A') {
+            throw new IllegalArgumentException("The diamond shape should only be within the alphabet [A-Z]. Supplied value: " + letter);
+        }
+
+        if (letter == 'A') {
+            return "A";
+        }
+
+        // figure out how wide the middle of the diamond is
+        int midLevel = letter - 'A'; // B is at level 1, C is at level 2 etc
+        int diamondWidth = 2 * midLevel + 1;
+        // each level will have a certain amount of leading and trailing spaces (equal amount) as well as middle spaces
+        // we can calculate based on level what the middle space count is and then we have everything:
+        //    1. leading + char+ trailing or
+        //    2. leading + char + middle + char + trailing
+        StringBuilder result = new StringBuilder();
+        for (char currChar = 'A'; currChar <= letter; currChar++) {
+            addRow(currChar, diamondWidth, result);
+        }
+        for (char currChar = (char) (letter - 1); currChar >= 'A'; currChar--) {
+            addRow(currChar, diamondWidth, result);
+        }
+
+
+        return result.toString();
+    }
+
+    private void addRow(char c, int diamondWidth, StringBuilder result) {
+        int currlevel = c - 'A';
+        int middleSpaces = Math.max(2 * currlevel - 1, 0);
+        int charOccurence = middleSpaces > 0 ? 2 : 1;
+        int trailingSpaces = (diamondWidth - middleSpaces - charOccurence) / 2;
+
+        // process all spaces and characters to build a row
+
+
+        result.append(" ".repeat(trailingSpaces));
+        result.append(c);
+        //not the top of the diamond
+        if (charOccurence > 1) {
+            result.append(" ".repeat(middleSpaces));
+            result.append(c);
+        }
+        result.append(" ".repeat(trailingSpaces));
+        result.append("\n");
+    }
+}
---
+++ src/test/java/DiamondDrawerTest.java
@@ -0,0 +1,114 @@
+
+import org.example.DiamondDrawer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DiamondDrawerTest {
+
+    DiamondDrawer diamondDrawer;
+
+
+    @BeforeEach
+    public void setUp() {
+        diamondDrawer = new DiamondDrawer();
+    }
+
+    @Test
+    void testADiamond() {
+        String expected = "A";
+
+        String result = diamondDrawer.drawDiamond('A');
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+
+    @Test
+    void testBDiamond() {
+        String shape = """
+                _A_
+                B_B
+                _A_
+                """;
+        String expected = shape.replace("_", " ");
+
+        String result = diamondDrawer.drawDiamond('B');
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testCDiamond() {
+        String shape =
+                """
+                        __A__
+                        _B_B_
+                        C___C
+                        _B_B_
+                        __A__
+                        """;
+        String expected = shape.replace("_", " ");
+
+        String result = diamondDrawer.drawDiamond('C');
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testGDiamond() {
+        String shape =
+                """
+                        ______A______
+                        _____B_B_____
+                        ____C___C____
+                        ___D_____D___
+                        __E_______E__
+                        _F_________F_
+                        G___________G
+                        _F_________F_
+                        __E_______E__
+                        ___D_____D___
+                        ____C___C____
+                        _____B_B_____
+                        ______A______
+                        """;
+        String expected = shape.replace("_", " ");
+
+        String result = diamondDrawer.drawDiamond('G');
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testLowerCaseLetterFDiamond() {
+        String shape =
+                """
+                        _____A_____
+                        ____B_B____
+                        ___C___C___
+                        __D_____D__
+                        _E_______E_
+                        F_________F
+                        _E_______E_
+                        __D_____D__
+                        ___C___C___
+                        ____B_B____
+                        _____A_____
+                        """;
+        String expected = shape.replace("_", " ");
+
+        // I made it work for both lower and uppercase
+        String result = diamondDrawer.drawDiamond('f');
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testNonAlphabeticCharacter() {
+        assertThrows(IllegalArgumentException.class, () -> diamondDrawer.drawDiamond('#'));
+        assertThrows(IllegalArgumentException.class, () -> diamondDrawer.drawDiamond('2'));
+    }
+}
